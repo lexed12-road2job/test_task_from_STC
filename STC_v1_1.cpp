a20764cf78f3adf3840f5fbdd6df328d9dfa2e39
Daniil <AlyshevDE@yandex.ru>
2023-11-20 09:04:28 +0300

    Основные файлы

---
+++ STC_v1_1.cpp
@@ -0,0 +1,21 @@
+﻿// STC_v1_1.cpp: определяет точку входа для приложения.
+//
+
+#include "Smooth.h"
+#include "testbench.h"
+
+#include <iostream>
+#include <stdio.h> 
+#include "Smooth.h"
+#include <chrono>
+
+
+
+int main()
+{
+    run();
+    std::cout << "Hello World!\n";
+}
+
+
+
---
+++ Smooth.h
@@ -0,0 +1,86 @@
+﻿// STC_v1_1.h : включаемый файл для стандартных системных включаемых файлов
+// или включаемые файлы для конкретного проекта.
+
+#pragma once
+#include "Smooth.h"
+#include <iostream>
+
+#include "math.h"
+#include <stdio.h> 
+#include <time.h> 
+#include <chrono>
+
+void smooth(const double* input, double* output, const int n, const int window);
+void smooth(const float* input, float* output, const int n, const int window);
+
+void time_measuring(const float* input, float* output, const int n, const int window);
+void time_measuring(const double* input, double* output, const int n, const int window);
+
+void time_measuring(const float* input, float* output, const int n, const int window)
+{
+    auto start = std::chrono::steady_clock::now();
+
+    //  Insert the code that will be timed
+    smooth(input, output, n, window);
+    auto end = std::chrono::steady_clock::now();
+
+    // Store the time difference between start and end
+    auto diff = end - start;
+
+
+    std::cout << std::chrono::duration <double, std::milli>(diff).count() << " ms" << std::endl;
+}
+void time_measuring(const double* input, double* output, const int n, const int window)
+{
+    auto start = std::chrono::steady_clock::now();
+
+    //  Insert the code that will be timed
+    smooth(input, output, n, window);
+    auto end = std::chrono::steady_clock::now();
+
+    // Store the time difference between start and end
+    auto diff = end - start;
+
+
+    std::cout << std::chrono::duration <double, std::milli>(diff).count() << " ms" << std::endl;
+}
+
+void smooth(const double* input, double* output, const int n, const int window)
+{
+    const double wind_1 = pow(window, -1);
+
+    double tmp = 0;
+
+    for (int i = 0; i < window; i++)
+    {
+        tmp = tmp + input[i];                                   //Считаем среднее первого окна
+    }
+    output[0] = tmp * wind_1;
+
+    for (int i = window; i <= n; i++)
+    {
+        output[i - window + 1] = output[i - window] +
+            wind_1 * (input[i] - input[i - window]);            //Считаем среднее шагающего окна
+    }
+}
+void smooth(const float* input, float* output, const int n, const int window)
+{
+    const float wind_1 = powf(window, -1); //находим обратное значение числа элементов окна для оптимизации деления
+
+    float tmp = 0;
+
+
+    for (int i = 0; i < window; i++)
+    {
+        tmp = tmp + input[i];
+    }
+    output[0] = tmp * wind_1;
+
+    for (int i = window; i <= n; i++)
+    {
+        output[i - window + 1] = output[i - window] +
+            wind_1 * (input[i] - input[i - window]);
+    }
+}
+
+// TODO: установите здесь ссылки на дополнительные заголовки, требующиеся для программы.
